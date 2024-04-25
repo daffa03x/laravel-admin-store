4020d5657f713b4f285625d1c8aa7dd5b8733629
daffa03x <mobadaff@gmail.com>
2024-04-25 14:49:07 +0700

    fitur user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -21,7 +22,7 @@ class UserController extends Controller
      */
     public function create()
     {
-        //
+        return view('user.create');
     }
 
     /**
@@ -29,7 +30,22 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'name' =>'required',
+            'email' =>'required',
+            'password' => 'required',
+        ]);
+
+        $data = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+        if($data){
+            return redirect()->route('user.index')->with('success', 'Berhasil Tambah User');
+        }else{
+            return redirect()->route('user.create')->with('error', 'Gagal Tambah User');
+        }
     }
 
     /**
@@ -43,24 +59,46 @@ class UserController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(User $user)
+    public function edit($id)
     {
-        //
+        $data = User::find($id);
+        return view('user.edit',compact('data'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, User $user)
+    public function update(Request $request, $id)
     {
-        //
+        $data = User::find($id);
+        $request->validate([
+            'name' =>'required',
+            'email' =>'required',
+        ]);
+        $update = $data->update([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        if($update){
+            return redirect()->route('user.index')->with('success', 'Berhasil Ubah User');
+        }else{
+            return redirect()->route('user.edit',$id)->with('error', 'Gagal Ubah User');
+        }
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(User $user)
+    public function destroy($id)
     {
-        //
+        $data = User::find($id);
+        $delete = $data->delete();
+        if($delete){
+            return redirect()->route('user.index')->with('success', 'Berhasil Hapus User');
+        }else{
+            return redirect()->route('user.index')->with('error', 'Gagal Hapus User');
+        }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -19,5 +19,11 @@ Route::middleware('auth')->group(function () {
 });
 
 Route::get('/user', [UserController::class, 'index'])->name('user.index');
+Route::get('/user/create', [UserController::class, 'create'])->name('user.create');
+Route::get('/user/edit/{id}', [UserController::class, 'edit'])->name('user.edit');
+Route::post('/user/store', [UserController::class, 'store'])->name('user.store');
+Route::put('/user/update/{id}', [UserController::class, 'update'])->name('user.update');
+Route::get('/user/destroy/{id}', [UserController::class, 'destroy'])->name('user.destroy');
+
 
 require __DIR__.'/auth.php';
